4f43562841283d45f26e61c9d99abb81ef0d9683
Usman Gani Khan <ugkha11@gmail.com>
2020-07-14 20:05:34 +0530

    Second commit

--- src/main/java/com/demo/springbootbb/SpringbootBbApplication.java
+++ src/main/java/com/demo/springbootbb/SpringbootBbApplication.java
@@ -2,7 +2,7 @@ package com.demo.springbootbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+// 
 @SpringBootApplication
 public class SpringbootBbApplication {
 
