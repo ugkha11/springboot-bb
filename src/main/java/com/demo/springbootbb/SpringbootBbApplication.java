86c30e9ef1017e64899487ecdd7c5f5075241938
Usman Gani Khan <ugkha11@gmail.com>
2020-07-14 18:20:30 +0530

    First Commit

---
+++ src/main/java/com/demo/springbootbb/SpringbootBbApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBbApplication.class, args);
+	}
+
+}
