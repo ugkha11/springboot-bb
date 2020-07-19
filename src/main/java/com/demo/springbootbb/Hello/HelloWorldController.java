6536325f35500423fd91f8faa3226ab21fa1ac0d
Usman Gani Khan <ugkha11@gmail.com>
2020-07-20 00:36:36 +0530

    i18N First-Commit

--- src/main/java/com/demo/springbootbb/Hello/HelloWorldController.java
+++ src/main/java/com/demo/springbootbb/Hello/HelloWorldController.java
@@ -1,6 +1,12 @@
 package com.demo.springbootbb.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController
 {
+	
+	/*
+	 * @Autowired private ResourceBundleMessageSource messageSource;
+	 */
+	
+	
 	//simple method
 	//@RequestMapping(method = RequestMethod.GET,path="/helloworld1")
 	@GetMapping("/helloworld1")
@@ -22,5 +34,26 @@ public class HelloWorldController
 	{
 		return new UserDetails("Osman","Khan","Finland"); 
 	}
+	
+//	@GetMapping("/hello-int")
+//	public String getMessageInI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+//		
+//		//return messageSource.getMessage("label.hello",null, new Locale(locale));
+//		//return "Hello world i18N";
+//	}
+	
+	
+	//i18N
+	@Autowired
+    MessageSource messageSource;
+     
+    @GetMapping("/hello-int")
+    public String index(Locale locale) {
+        return messageSource.getMessage("label.hello", null, locale);
+    }
+	
+	
+	
+	
  
 }
--- src/main/java/com/demo/springbootbb/SpringbootBbApplication.java
+++ src/main/java/com/demo/springbootbb/SpringbootBbApplication.java
@@ -1,13 +1,36 @@
 package com.demo.springbootbb;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 // 
 @SpringBootApplication
 public class SpringbootBbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBbApplication.class, args);
+	}
+	
+    //@Bean
+	public LocaleResolver localeResolver() {
+		
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localeResolver();
+		
+	}
+	
+	//@Bean
+	public ResourceBundleMessageSource messageSource() {
+		
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
