c1fe415fadaf4c15baf6ab2cf0737e43123cc595
Usman Gani Khan <ugkha11@gmail.com>
2020-07-23 22:10:01 +0530

    Micrometer First-Commit

---
+++ src/main/java/com/demo/springbootbb/config/MonitoringConfig.java
@@ -0,0 +1,30 @@
+package com.demo.springbootbb.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.appoptics.AppOpticsConfig;
+import io.micrometer.appoptics.AppOpticsMeterRegistry;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.lang.Nullable;
+
+@Configuration
+public class MonitoringConfig {
+	
+	AppOpticsConfig appopticsConfig = new AppOpticsConfig() {
+	    @Override
+	    public String apiToken() {
+	        return "asdflkj";
+	    }
+
+	    @Override
+	    @Nullable
+	    public String get(String k) {
+	        return null;
+	    }
+	};
+	MeterRegistry registry = new AppOpticsMeterRegistry(appopticsConfig, Clock.SYSTEM);
+	
+	
+
+}
